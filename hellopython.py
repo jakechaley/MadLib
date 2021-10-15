ecc2ff7a1d2f42b9f81f4864907beb3b7e0f4150
jakechaley <jakechaley@gmail.com>
2021-10-14 23:03:30 -0700

    add the correct file to the repository

---
+++ hellopython.py
@@ -0,0 +1,4 @@
+noun = input("Enter an animal ")
+noun2 = input("Enter plural noun ")
+noun3 = input("Enter a noun ")
+print("My programming goal is to become a coding " + noun + ". Once I achieve that I will gain many " + noun2 + ". I would love to program a " + noun3 + " one day.")
